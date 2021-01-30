406d4281ec9f561412e4651613cb07854392814b
petterigit <33552590+petterigit@users.noreply.github.com>
2021-01-30 16:56:02 +0200

    Multiplayer Support

---
+++ Assets/Scripts/menuInputListener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class menuInputListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<TextMeshPro> playerStatusPrompts;
+    //public List<string> playerAction1Buttons;
+    public List<KeyCode> playerKeyCodes;
+
+    private int index;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        index = 0;
+        foreach (KeyCode buttonKey in playerKeyCodes) {
+            if (Input.GetKeyDown(buttonKey)){
+                
+                /* spawnSucces = Spawner.spawn(index) */
+
+                bool spawnSuccess = true;
+                if ( spawnSuccess ) {
+                    changeStatusText(index);
+                }
+                index++;
+            }
+        }
+    }
+    void changeStatusText(int index) {
+        playerStatusPrompts[index].text = "Player " + (index+1) + " is ready!";
+    }
+}
