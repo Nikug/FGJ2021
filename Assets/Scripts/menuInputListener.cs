a0cc41a5995f3f281e07cfbfcfa82bc11780e670
nikugronberg <niku.gronberg@student.lut.fi>
2021-01-30 18:02:38 +0200

    fix joining controllers

--- Assets/Scripts/menuInputListener.cs
+++ Assets/Scripts/menuInputListener.cs
@@ -3,37 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class menuInputListener : MonoBehaviour
+public class MenuInputListener : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public List<TextMeshPro> playerStatusPrompts;
     //public List<string> playerAction1Buttons;
-    public List<KeyCode> playerKeyCodes;
+    public List<string> playerJoinKey;
+    public List<string> playerLeaveKey;
+    public List<string> joinedPlayers;
 
     private int index;
     void Start()
     {
+        index = 0;
+        playerJoinKey = new List<string>();
+        playerLeaveKey = new List<string>();
+        for(int i = 0; i < 4; i++) {
+            playerJoinKey.Add("Joy" + (i +1) + "Action1");
+        }
+        for(int i = 0; i < 4; i++) {
+            playerLeaveKey.Add("Joy" + (i +1) + "Action2");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        index = 0;
-        foreach (KeyCode buttonKey in playerKeyCodes) {
-            if (Input.GetKeyDown(buttonKey)){
+        foreach (string buttonKey in playerJoinKey) {
+            if (Input.GetButtonDown(buttonKey)){
+                string name = buttonKey.Substring(3, 1);
+                if(!joinedPlayers.Contains(name)) {
+                    joinedPlayers.Add(name);
+                    changeStatusText(int.Parse(name) - 1, name, true);
+                }
                 
                 /* spawnSucces = Spawner.spawn(index) */
 
-                bool spawnSuccess = true;
-                if ( spawnSuccess ) {
-                    changeStatusText(index);
+            }
+        }
+
+        foreach (string buttonKey in playerLeaveKey) {
+            if (Input.GetButtonDown(buttonKey)){
+                string name = buttonKey.Substring(3, 1);
+                if(joinedPlayers.Contains(name)) {
+                    joinedPlayers.Remove(name);
+                    changeStatusText(int.Parse(name) - 1, name, false);
                 }
-                index++;
+                
+                /* spawnSucces = Spawner.spawn(index) */
+
             }
         }
     }
-    void changeStatusText(int index) {
-        playerStatusPrompts[index].text = "Player " + (index+1) + " is ready!";
+    void changeStatusText(int index, string player, bool joined) {
+        if(joined) {
+            playerStatusPrompts[index].text = "Player " + player + " is ready!";
+        } else {
+            playerStatusPrompts[index].text = "Press A to join";
+        }
     }
 }
