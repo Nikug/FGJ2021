833ad048298a37cd8dd36186c5c084cfb4ba7a9d
nikugronberg <niku.gronberg@student.lut.fi>
2021-01-31 01:19:49 +0200

    ui stub

---
+++ Assets/Scripts/UICOntroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICOntroller : MonoBehaviour
+{
+    private GameController gameController;
+
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    public void OpenMenu()
+    {
+
+    }
+}
