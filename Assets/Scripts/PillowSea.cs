b6e3ac239caaca3384a8f5dc865963b51a3c9439
nikugronberg <niku.gronberg@student.lut.fi>
2021-01-30 16:58:24 +0200

    add eating children

---
+++ Assets/Scripts/PillowSea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillowSea : MonoBehaviour
+{
+    public string toddlerTag;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == toddlerTag)
+        {
+            other.gameObject.AddComponent<ConsumeChild>();
+        }
+    }
+}
