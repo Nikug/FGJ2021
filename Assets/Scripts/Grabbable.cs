af089a886acc85ef34ac22155bdb51ae7e2736d6
nikugronberg <niku.gronberg@student.lut.fi>
2021-01-30 21:59:36 +0200

    change some player stuff

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -7,7 +7,8 @@ public class Grabbable : MonoBehaviour
     public bool grabbed;
     public string lastToucher;
 
-    public void grab(string playerName) {
+    public void grab(string playerName)
+    {
         var collider = GetComponent<CapsuleCollider>();
         collider.enabled = false;
         var agent = GetComponent<MoveTo>();
@@ -15,10 +16,11 @@ public class Grabbable : MonoBehaviour
         lastToucher = playerName;
     }
 
-    public void free() {
+    public void free()
+    {
         var collider = GetComponent<CapsuleCollider>();
         collider.enabled = true;
         var agent = GetComponent<MoveTo>();
-        agent.EnableAgent();
+        agent.enableAgentOnTouch = true;
     }
 }
